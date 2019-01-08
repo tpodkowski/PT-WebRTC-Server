2878286c9594b70c1d49ab83baff7ed3c09fe237
Tomasz Podkowski <t.podkowski@merixstudio.com>
2019-01-08 21:55:47 +0100

    Fix statics.

--- src/app.js
+++ src/app.js
@@ -3,7 +3,7 @@ const http = require('http').Server(app);
 const io = require('socket.io')(http);
 const app = express();
 
-app.use(express.static('public'));
+app.use(express.static(__dirname + '/public'));
 
 let history = [];
 
